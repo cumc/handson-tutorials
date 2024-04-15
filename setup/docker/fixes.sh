8d3d28370edf350b7b1d2dcde0e490ad5da0addc
danielnachun <daniel.nachun@gmail.com>
2024-04-15 00:19:20 -0700

    add perl to global packages

--- setup/docker/fixes.sh
+++ setup/docker/fixes.sh
@@ -4,6 +4,3 @@ apt-get -y install libgl1 libgomp1
 
 # pixi global mistakenly points the samtools wrapper to samtools.pl, so we need to revert this change
 #sed -i "s/samtools.pl/samtools/" /root/.pixi/bin/samtools
-
-# install perl modules
-apt-get install --yes perl-modules
