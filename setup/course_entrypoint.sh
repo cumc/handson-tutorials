9546f1d7a93eacf4f76bf56809d05c737d6eeb6c
Gao Wang <wangow@gmail.com>
2024-04-15 22:44:53 -0400

    Remove outputs from notebooks

--- setup/course_entrypoint.sh
+++ setup/course_entrypoint.sh
@@ -4,7 +4,7 @@ repo_dir=handson-tutorials
 BUCKET_ACCESS_KEY=${BUCKET_ACCESS_KEY:-""}
 BUCKET_SECRET_KEY=${BUCKET_SECRET_KEY:-""}
 
-cd /root/
+cd
 
 # Clone GitHub repo
 git clone https://github.com/cumc/handson-tutorials.git
@@ -24,6 +24,10 @@ curl -o "$repo_dir/contents/ngs_qc_annotation/pipelines/genotype_formatting.ipyn
 curl -o "$repo_dir/contents/ngs_qc_annotation/pipelines/GWAS_QC.ipynb" https://raw.githubusercontent.com/cumc/xqtl-protocol/main/code/data_preprocessing/genotype/GWAS_QC.ipynb
 curl -o "$repo_dir/contents/ngs_qc_annotation/pipelines/PCA.ipynb" https://raw.githubusercontent.com/cumc/xqtl-protocol/main/code/data_preprocessing/genotype/PCA.ipynb
 curl -o "$repo_dir/contents/ngs_qc_annotation/pipelines/annovar.ipynb" https://raw.githubusercontent.com/cumc/bioworkflows/master/variant-annotation/annovar.ipynb
+
+# Remove outputs from notebooks
+find "$repo_dir/contents" -type f -name "*.ipynb" | parallel -j $(nproc) "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {}"
+
 
 # Sync the contents directory
 AWS_ACCESS_KEY_ID=$BUCKET_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$BUCKET_SECRET_KEY aws s3 sync s3://opcenter-bucket-ada686a0-ccdb-11ee-b922-02ebafc2e5cf/tutorial_data /root/handson-tutorials/contents
