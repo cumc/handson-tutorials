c560c555696b116a6389caa3c7ef2d7e1a555f8e
gaow <wangow@gmail.com>
2024-04-16 20:17:42 -0400

    Update course_entrypoint.sh

--- setup/course_entrypoint.sh
+++ setup/course_entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-repo_dir=handson-tutorials
+repo_dir=$HOME/handson-tutorials
 BUCKET_ACCESS_KEY=${BUCKET_ACCESS_KEY:-""}
 BUCKET_SECRET_KEY=${BUCKET_SECRET_KEY:-""}
 
-cd
-
 # Clone GitHub repo
-git clone https://github.com/cumc/handson-tutorials.git
+cd && git clone https://github.com/cumc/handson-tutorials.git
 
 # Download external resources
 mkdir -p $repo_dir/contents/ldpred2
@@ -29,12 +27,12 @@ curl -o "$repo_dir/contents/ngs_qc_annotation/pipelines/annovar.ipynb" https://r
 mkdir -p ~/.parallel && touch ~/.parallel/will-cite
 find "$repo_dir/contents" -type f -name "*.ipynb" | parallel -j $(nproc) "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {}"
 
+# Prepare the get-data command
+echo -e "#!/bin/bash\n" > $HOME/.pixi/bin/get-data && chmod +x $HOME/.pixi/bin/get-data
+echo "synapse get -r syn36416559 --downloadLocation $repo_dir/contents" >> $HOME/.pixi/bin/get-data
 
-# Sync the contents directory
-AWS_ACCESS_KEY_ID=$BUCKET_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$BUCKET_SECRET_KEY aws s3 sync s3://opcenter-bucket-ada686a0-ccdb-11ee-b922-02ebafc2e5cf/tutorial_data /home/jovyan/handson-tutorials/contents
-
-# Sync the annovar software
-(AWS_ACCESS_KEY_ID=$BUCKET_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$BUCKET_SECRET_KEY aws s3 sync s3://opcenter-bucket-ada686a0-ccdb-11ee-b922-02ebafc2e5cf/annovar_software/ /home/jovyan/.pixi/bin --exclude "*" --include "*.pl" && chmod +x /home/jovyan/.pixi/bin/*.pl) || (echo -e "\033[1;31mWarning: Cannot install ANNOVAR program due to license restriction. Exercise involving ANNOVAR annotations will not work unless you manually install ANNOVAR to the /home/jovyan/.pixi/bin folder of the tutorials.\033[0m" && true)
+# Sync necessary resources: annovar software and data
+(AWS_ACCESS_KEY_ID=$BUCKET_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$BUCKET_SECRET_KEY aws s3 sync s3://opcenter-bucket-ada686a0-ccdb-11ee-b922-02ebafc2e5cf/annovar_software/ /home/jovyan/.pixi/bin --exclude "*" --include "*.pl" && chmod +x /home/jovyan/.pixi/bin/*.pl && get-data) || (echo -e "\033[1;31mWarning: Cannot install ANNOVAR program due to license restriction. Exercise involving ANNOVAR annotations will not work unless you manually install ANNOVAR to the /home/jovyan/.pixi/bin folder of the tutorials.\033[0m" && true)
 
 # Fix plink.multivariate
 mv /home/jovyan/handson-tutorials/contents/archive/plink.multivariate /home/jovyan/.pixi/bin && chmod +x /home/jovyan/.pixi/bin/plink.multivariate
