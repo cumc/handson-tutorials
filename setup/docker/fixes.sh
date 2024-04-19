10d1392e896a06c5d8a927d0f8f1855871ba0452
gaow <wangow@gmail.com>
2024-04-19 17:41:55 -0400

    Update fixes.sh

--- setup/docker/fixes.sh
+++ setup/docker/fixes.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 # pixi global mistakenly points the samtools wrapper to samtools.pl, so we need to revert this change
 #sed -i "s/samtools.pl/samtools/" /root/.pixi/bin/samtools
+
+# Install & redistribute ANNOVAR for educational purpose, with permission granted by the author of ANNOVAR on April 19, 2024
+curl https://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz -o - | tar zxvf - --strip-components=1
