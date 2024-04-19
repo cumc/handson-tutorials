a57c43147d2ce68ccacd769327135a16fda70dbf
gaow <wangow@gmail.com>
2024-04-19 17:42:50 -0400

    Update fixes.sh

--- setup/docker/fixes.sh
+++ setup/docker/fixes.sh
@@ -3,4 +3,4 @@
 #sed -i "s/samtools.pl/samtools/" /root/.pixi/bin/samtools
 
 # Install & redistribute ANNOVAR for educational purpose, with permission granted by the author of ANNOVAR on April 19, 2024
-curl https://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz -o - | tar zxvf - --strip-components=1
+curl https://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz -o - | tar zxvf - --strip-components=1 -C $HOME/.pixi/bin
