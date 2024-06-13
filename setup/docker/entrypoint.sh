dd0ba39ac547f757022603028d2950d5e9dc7c2f
gaow <wangow@gmail.com>
2024-06-13 13:07:01 -0400

    Update entrypoint.sh

--- setup/docker/entrypoint.sh
+++ setup/docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+cd /home/jovyan/
 curl -fsSL https://raw.githubusercontent.com/cumc/handson-tutorials/main/setup/course_entrypoint.sh | bash
 cd /home/jovyan/handson-tutorials/contents
 jupyter-lab
