91d621a6766798c41abc6d450cf1e779624a06eb
gaow <wangow@gmail.com>
2025-06-23 04:17:25 +0800

    Update course_entrypoint.sh

--- setup/course_entrypoint.sh
+++ setup/course_entrypoint.sh
@@ -28,15 +28,15 @@ mkdir -p ~/.parallel && touch ~/.parallel/will-cite
 find "$repo_dir/contents" -type f -name "*.ipynb" | parallel -j $(nproc) "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {}"
 
 # Prepare the get-data command
-ln -s $repo_dir/setup/.synapseConfig $HOME/.synapseConfig
-echo -e "#!/bin/bash\n" > $HOME/bin/get-data && chmod +x $HOME/bin/get-data
-echo "synapse get -r syn18700992 --downloadLocation $repo_dir/contents" >> $HOME/bin/get-data
+# ln -s $repo_dir/setup/.synapseConfig $HOME/.synapseConfig
+# echo -e "#!/bin/bash\n" > $HOME/bin/get-data && chmod +x $HOME/bin/get-data
+# echo "synapse get -r syn18700992 --downloadLocation $repo_dir/contents" >> $HOME/bin/get-data
 #get-data
 
 # Download data-set
-BUCKET_ACCESS_KEY=${BUCKET_ACCESS_KEY:-""}
-BUCKET_SECRET_KEY=${BUCKET_SECRET_KEY:-""}
-AWS_ACCESS_KEY_ID=$BUCKET_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$BUCKET_SECRET_KEY aws s3 sync s3://rockefeller-course/AGIS/ $HOME/handson-tutorials/contents --exclude "*.ipynb"
+# BUCKET_ACCESS_KEY=${BUCKET_ACCESS_KEY:-""}
+# BUCKET_SECRET_KEY=${BUCKET_SECRET_KEY:-""}
+# AWS_ACCESS_KEY_ID=$BUCKET_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$BUCKET_SECRET_KEY aws s3 sync s3://rockefeller-course/AGIS/ $HOME/handson-tutorials/contents --exclude "*.ipynb"
 
 # Fix plink.multivariate
 mv $repo_dir/contents/archive/plink.multivariate $HOME/bin && chmod +x $HOME/bin/plink.multivariate
@@ -45,4 +45,4 @@ mv $repo_dir/contents/archive/plink.multivariate $HOME/bin && chmod +x $HOME/bin
 curl https://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz -o - | tar zxvf - --strip-components=1 -C $HOME/bin
 
 cd $HOME/handson-tutorials/contents
-jupyter-lab
+# jupyter-lab
